7cd86b82f00131c14a9141dfbc69c944d2fd324c
NearLawiet <sooraj1999vs@gmail.com>
2020-01-14 12:51:12 +0530

    DDA Circle Remains

--- a02/mainwindow.cpp
+++ a02/mainwindow.cpp
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 }
-void MainWindow::bresenhamCircle(){
+void MainWindow::bresenhamCircle(int radius){
     int x=0,y=radius;
     int d=3-2*radius;
     drawCircle(x,y);
@@ -34,6 +34,40 @@ void MainWindow::drawCircle(int x,int y){
     image.setPixel(xc-y,yc+x,green);
     image.setPixel(xc+y,yc-x,green);
     image.setPixel(xc-y,yc-x,green);
+
+    image.setPixel(xc,yc,green);
+    ui->label->setPixmap(QPixmap::fromImage(image));
+    ui->label->show();
+}
+void MainWindow::drawTriangle(){
+    int x1=xc+0;
+    int y1=yc-radius;
+    int x2=xc+radius*0.866;
+    int y2=yc+radius*0.5;
+    int x3=xc-radius*0.866;
+    int y3=yc+radius*0.5;
+    DDALine(x1,y1,x2,y2);
+    DDALine(x3,y3,x2,y2);
+    DDALine(x1,y1,x3,y3);
+
+}
+void MainWindow::DDALine(int x1, int y1, int x2, int y2){
+    float dx,dy,steps,xinc,yinc;
+    dx=x2-x1;
+    dy=y2-y1;
+
+    steps=abs(dx)>abs(dy)?abs(dx):abs(dy);
+    xinc=dx/steps;
+    yinc=dy/steps;
+
+    float x=x1;
+    float y=y1;
+
+    for(int i=0;i<=steps;i++){
+        image.setPixel(x,y,green);
+        x=x+xinc;
+        y=y+yinc;
+    }
     ui->label->setPixmap(QPixmap::fromImage(image));
     ui->label->show();
 }
@@ -50,5 +84,7 @@ void MainWindow::on_textEdit_textChanged()
 
 void MainWindow::on_pushButton_clicked()
 {
-    bresenhamCircle();
+    bresenhamCircle(radius);
+    drawTriangle();
+    bresenhamCircle(radius/2);
 }
--- a02/mainwindow.h
+++ a02/mainwindow.h
@@ -13,8 +13,10 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    void bresenhamCircle();
+    void bresenhamCircle(int);
     void drawCircle(int,int);
+    void drawTriangle();
+    void DDALine(int,int,int,int);
     ~MainWindow();
 
 private slots:
